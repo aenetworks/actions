3d48738b0059f6d91263ed969ddf31599a1c5a13
Pawel Drozd <pawel.drozd@aenetworks.com>
2021-10-15 22:14:02 +0200

    .

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -1,9 +1,29 @@
 abstract class ErrorFactory<T = any> extends Error {
+  private readonly _msg = '';
+
   constructor(args?: T) {
     super();
     Object.setPrototypeOf(this, new.target.prototype);
     this.name = this.constructor.name;
-    this.message = this.constructor['msg'];
+    this._msg = this.constructor['msg'];
+
+    this.message = this.getMessage(args);
+  }
+
+  protected getMessage(args?: T): string {
+    if (!!this._msg && !!args) {
+      return `${this._msg}: ${args}.`;
+    }
+
+    if (!!this._msg) {
+      return `${this._msg}.`;
+    }
+
+    if (!!args) {
+      return `${args}.`;
+    }
+
+    return '';
   }
 }
 
