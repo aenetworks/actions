126aa80622a687812d70e690cc57271aea56b526
Pawel Drozd <pawel.drozd@aenetworks.com>
2021-10-26 17:59:07 +0200

    fix postman action

--- dist/lib/postman.js
+++ dist/lib/postman.js
@@ -24,6 +24,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const newman = __importStar(require("newman"));
 const decorators_1 = require("./decorators");
@@ -34,7 +43,10 @@ var PostmanErrors;
     PostmanErrors["ENVIRONMENT_LOAD_ERROR_MESSAGE"] = "could not load environment";
     PostmanErrors["LOAD_ERROR_MESSAGE"] = "could not load ";
 })(PostmanErrors || (PostmanErrors = {}));
+const Done = 'OK';
 class PostmanApiError extends seedWorks_1.ErrorBase {
+}
+class PostmanError extends seedWorks_1.ErrorBase {
 }
 /**
  * Build package.
@@ -52,29 +64,44 @@ class Postman {
      * Run command.
      */
     run() {
-        const options = {
-            collection: this._getCollectionUrl(),
-            environment: this._getEnvironmentUrl(),
-            reporters: 'cli',
-        };
-        newman.run(options, (err) => {
-            if (err) {
-                const errorDescription = err.message.split('\n');
-                console.warn(JSON.stringify({
-                    msg: errorDescription,
-                }));
-                if (errorDescription[0] === PostmanErrors.COLLECTION_LOAD_ERROR_MESSAGE) {
-                    throw new PostmanApiError(`Collection "${this.collectionId}" cannot be loaded. Check if it exists.`);
-                }
-                else if (errorDescription[0] === PostmanErrors.ENVIRONMENT_LOAD_ERROR_MESSAGE) {
-                    throw new PostmanApiError(`Environment "${this.environmentId}" cannot be loaded. Check if it exists.`);
-                }
-                else if (errorDescription[0] === PostmanErrors.LOAD_ERROR_MESSAGE) {
-                    throw new PostmanApiError('It cannot be loaded. Check if api key is valid.');
-                }
-                throw err;
-            }
+        return __awaiter(this, void 0, void 0, function* () {
+            const options = {
+                collection: this._getCollectionUrl(),
+                environment: this._getEnvironmentUrl(),
+                reporters: 'cli',
+            };
+            yield this._newmanRun(options);
         });
+    }
+    _newmanRun(options) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return new Promise((resolve, reject) => {
+                newman.run(options, (err) => {
+                    if (err) {
+                        reject(this._handleError(err));
+                    }
+                    resolve(Done);
+                });
+            });
+        });
+    }
+    _handleError(err) {
+        const errorDescription = err.message.split('\n');
+        console.warn(JSON.stringify({
+            msg: errorDescription,
+        }));
+        if (errorDescription[0] === PostmanErrors.COLLECTION_LOAD_ERROR_MESSAGE) {
+            return new PostmanApiError(`Collection "${this.collectionId}" cannot be loaded. Check if it exists.`);
+        }
+        else if (errorDescription[0] === PostmanErrors.ENVIRONMENT_LOAD_ERROR_MESSAGE) {
+            return new PostmanApiError(`Environment "${this.environmentId}" cannot be loaded. Check if it exists.`);
+        }
+        else if (errorDescription[0] === PostmanErrors.LOAD_ERROR_MESSAGE) {
+            return new PostmanApiError('It cannot be loaded. Check if api key is valid.');
+        }
+        else {
+            return new PostmanError(err.message);
+        }
     }
     _getCollectionUrl() {
         return `${this.apiUrl}/collections/${this.collectionId}?apikey=${this.apiKey}`;
--- src/lib/postman.ts
+++ src/lib/postman.ts
@@ -9,7 +9,17 @@ enum PostmanErrors {
   LOAD_ERROR_MESSAGE = 'could not load ',
 }
 
+interface NewmanOptions {
+  collection: string;
+  environment: string;
+  reporters: string;
+}
+
+const Done = 'OK';
+
 class PostmanApiError extends ErrorBase {}
+
+class PostmanError extends ErrorBase {}
 
 /**
  * Build package.
@@ -29,34 +39,46 @@ export default class Postman implements Command {
    * Run command.
    */
   @logGroup('Postman')
-  public run(): void {
-    const options = {
+  public async run(): Promise<void> {
+    const options: NewmanOptions = {
       collection: this._getCollectionUrl(),
       environment: this._getEnvironmentUrl(),
       reporters: 'cli',
     };
 
-    newman.run(options, (err) => {
-      if (err) {
-        const errorDescription = err.message.split('\n');
+    await this._newmanRun(options);
+  }
 
-        console.warn(
-          JSON.stringify({
-            msg: errorDescription,
-          })
-        );
-
-        if (errorDescription[0] === PostmanErrors.COLLECTION_LOAD_ERROR_MESSAGE) {
-          throw new PostmanApiError(`Collection "${this.collectionId}" cannot be loaded. Check if it exists.`);
-        } else if (errorDescription[0] === PostmanErrors.ENVIRONMENT_LOAD_ERROR_MESSAGE) {
-          throw new PostmanApiError(`Environment "${this.environmentId}" cannot be loaded. Check if it exists.`);
-        } else if (errorDescription[0] === PostmanErrors.LOAD_ERROR_MESSAGE) {
-          throw new PostmanApiError('It cannot be loaded. Check if api key is valid.');
+  private async _newmanRun(options: NewmanOptions): Promise<any> {
+    return new Promise((resolve, reject) => {
+      newman.run(options, (err) => {
+        if (err) {
+          reject(this._handleError(err));
         }
 
-        throw err;
-      }
+        resolve(Done);
+      });
     });
+  }
+
+  private _handleError(err: Error): PostmanApiError | PostmanError {
+    const errorDescription = err.message.split('\n');
+
+    console.warn(
+      JSON.stringify({
+        msg: errorDescription,
+      })
+    );
+
+    if (errorDescription[0] === PostmanErrors.COLLECTION_LOAD_ERROR_MESSAGE) {
+      return new PostmanApiError(`Collection "${this.collectionId}" cannot be loaded. Check if it exists.`);
+    } else if (errorDescription[0] === PostmanErrors.ENVIRONMENT_LOAD_ERROR_MESSAGE) {
+      return new PostmanApiError(`Environment "${this.environmentId}" cannot be loaded. Check if it exists.`);
+    } else if (errorDescription[0] === PostmanErrors.LOAD_ERROR_MESSAGE) {
+      return new PostmanApiError('It cannot be loaded. Check if api key is valid.');
+    } else {
+      return new PostmanError(err.message);
+    }
   }
 
   private _getCollectionUrl(): string {
